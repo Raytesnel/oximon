8409e3504d594eeeda0bf4e742d2b985ab2afa47
Lavinia <lavinia@Lavinias-MacBook-Air.local>
2025-07-29 22:57:31 +0200

    oefenen organic grass and bloem rotatie frames.

--- assets/tiles/TilesetNature.tsx
+++ assets/tiles/TilesetNature.tsx
@@ -1,4 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="TilesetNature" tilewidth="16" tileheight="16" tilecount="504" columns="24">
  <image source="TilesetNature.png" width="384" height="336"/>
+ <tile id="495">
+  <animation>
+   <frame tileid="268" duration="100"/>
+   <frame tileid="269" duration="100"/>
+   <frame tileid="270" duration="500"/>
+  </animation>
+ </tile>
 </tileset>
