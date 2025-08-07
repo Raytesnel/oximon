aa35adcba19c8589e5d0faddcf18d2e3c74d056b
Lavinia <lavinia@Lavinias-MacBook-Air.local>
2025-08-07 22:10:54 +0200

    clean up house

---
+++ assets/tiles/Animated/Waterfall/MiddleSheet16x16.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="MiddleSheet16x16" tilewidth="16" tileheight="16" tilecount="5" columns="5">
+ <image source="MiddleSheet16x16.png" width="80" height="16"/>
+</tileset>
