3e806e6be374bc4640865960eaecd80a37c647f9
raytesnel <raymondhouwing@gmail.com>
2026-04-12 23:15:14 +0200

    add facing direction

--- src/combat/mod.rs
+++ src/combat/mod.rs
@@ -10,13 +10,12 @@ pub struct CombatPlugin;
 
 impl Plugin for CombatPlugin {
     fn build(&self, app: &mut App) {
-        app.add_message::<events::DamageEvent>()
-            .add_systems(
-                Update,
-                (
-                    player_attack_input_system,
-                    apply_damage_system.after(player_attack_input_system),
-                ),
-            );
+        app.add_message::<events::DamageEvent>().add_systems(
+            Update,
+            (
+                player_attack_input_system,
+                apply_damage_system.after(player_attack_input_system),
+            ),
+        );
     }
 }
