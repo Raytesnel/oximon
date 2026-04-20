9946b899351fcc1d096ffb69610fbe65a7db4fc3
raytesnel <raymondhouwing@gmail.com>
2026-04-20 23:02:45 +0200

    ai moving

--- src/combat/mod.rs
+++ src/combat/mod.rs
@@ -2,9 +2,11 @@ mod attacks;
 pub mod components;
 pub mod events;
 pub mod systems;
+pub mod ai;
 mod tests;
 
 use crate::combat::components::AttackEvent;
+use crate::combat::ai::*;
 use crate::combat::events::DamageEvent;
 use bevy::prelude::*;
 use systems::*;
@@ -15,6 +17,11 @@ impl Plugin for CombatPlugin {
     fn build(&self, app: &mut App) {
         app.add_message::<DamageEvent>();
         app.add_message::<AttackEvent>();
+        app.add_systems(Update,(
+            apply_damage_system,
+            despawn_dead_system,
+            tick_hitstun,
+        ));
         app.add_systems(
             Update,
             (
@@ -24,12 +31,14 @@ impl Plugin for CombatPlugin {
                 attack_start_system,
                 attack_lifetime_system,
                 attack_hit_system,
-                apply_damage_system,
-                despawn_dead_system,
                 attack_follow_system,
                 cooldown_tick_system,
-                tick_hitstun,
-            ).run_if(not_in_hitstop),
+            ).run_if(not_in_hitstop)
         );
+        app.add_systems(Update, (
+            ai_decision_system,
+            ai_movement_system,
+            ai_attack_system,
+        ).chain());
     }
 }
