74c86d7f5fad02cca24cb4dcaf21327676410c5e
raytesnel <raymondhouwing@gmail.com>
2026-05-08 09:47:41 +0200

    add new assets

---
+++ assets/map/TX Tileset Wall.tsx
@@ -0,0 +1,89 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="TX Tileset Wall" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="../tiles/dungeon/TX Tileset Wall.png" width="512" height="512"/>
+ <tile id="20">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="24.4482" y="0.23284" width="8.14941" height="31.5886"/>
+  </objectgroup>
+ </tile>
+ <tile id="21">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.155227" y="0.155227" width="32.5976" height="31.9767"/>
+  </objectgroup>
+ </tile>
+ <tile id="22">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0776134" y="0.310454" width="32.2872" height="31.6663"/>
+  </objectgroup>
+ </tile>
+ <tile id="23">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0776134" y="0" width="32.2872" height="31.7439"/>
+  </objectgroup>
+ </tile>
+ <tile id="24">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0776134" y="0.155227" width="7.76134" height="31.8991"/>
+  </objectgroup>
+ </tile>
+ <tile id="25">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="-0.543294" y="-0.155227" width="8.14941" height="31.8991"/>
+  </objectgroup>
+ </tile>
+ <tile id="26">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="24.2154" y="-0.155227" width="7.68373" height="32.2096"/>
+  </objectgroup>
+ </tile>
+ <tile id="36">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="24.6034" y="-0.0776134" width="7.14043" height="31.8215"/>
+  </objectgroup>
+ </tile>
+ <tile id="37">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="-0.0776134" width="32.7528" height="31.8215"/>
+  </objectgroup>
+ </tile>
+ <tile id="38">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="-0.0776134" width="32.6752" height="31.8991"/>
+  </objectgroup>
+ </tile>
+ <tile id="39">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.23284" y="0" width="31.8991" height="31.8991"/>
+  </objectgroup>
+ </tile>
+ <tile id="40">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.388067" y="0.155227" width="7.91657" height="31.511"/>
+  </objectgroup>
+ </tile>
+ <tile id="41">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-2.25079" y="-0.310454" width="9.77929" height="32.0543"/>
+  </objectgroup>
+ </tile>
+ <tile id="42">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="24.3706" y="0" width="7.21805" height="31.8991"/>
+  </objectgroup>
+ </tile>
+ <tile id="56">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="-0.155227" width="7.60611" height="31.9767"/>
+  </objectgroup>
+ </tile>
+ <tile id="57">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-1.39704" y="-0.23284" width="9.00315" height="32.2872"/>
+  </objectgroup>
+ </tile>
+ <tile id="58">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="24.6034" y="0.23284" width="7.29566" height="31.5886"/>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ assets/map/block-0003.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="block-0003" tilewidth="32" tileheight="46" tilecount="2" columns="2">
+ <image source="../sprites/objects/block-0003.png" width="64" height="46"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.10485" y="14.05" width="31.8745" height="31.56"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.10485" y="14.05" width="31.8745" height="31.56"/>
+  </objectgroup>
+ </tile>
+</tileset>
